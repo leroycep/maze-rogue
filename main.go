cc9dfa37770f06b69711bad7464f7fd3094fe645
geemili <minergeemili@gmail.com>
2016-01-25 10:03:47 -0600

    Added in glfw

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-gl/glfw/v3.1/glfw"
+	"runtime"
 )
+
+func init() {
+	runtime.LockOSThread()
+}
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
