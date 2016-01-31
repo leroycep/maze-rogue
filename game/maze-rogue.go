623e8ac557f73247b8ca774f48bc594b35d93a34
geemili <minergeemili@gmail.com>
2016-01-31 16:56:52 -0600

    Added in player

--- game/maze-rogue.go
+++ game/maze-rogue.go
@@ -7,8 +7,9 @@ import (
 )
 
 type GameData struct {
-	Width, Height int
-	Tiles         []int
+	Width, Height    int
+	Tiles            []int
+	PlayerX, PlayerY int
 }
 
 var game GameData
@@ -16,21 +17,22 @@ var game GameData
 func Init() {
 	width, height, regionid := 80, 60, 1
 
-	gl.Ortho(0, float64(width), 0, float64(height), -1, 3)
+	gl.Ortho(0, 40, 0, 30, -1, 3)
 
 	rooms := generate.PlaceRooms(width, height, 100, 2, 10)                    // Place rooms between 3x3 and 5x5 in a 40 x 30 grid of tiles
 	bakedRooms, regionid := generate.BakeRooms(rooms, width, height, regionid) // Render rooms down to a grid
 	maze, regionid := generate.MakeMazes(bakedRooms, width, height, regionid)  // Finish up by generating mazes between rooms
 	connect, regionid := generate.ConnectRooms(maze, width, height, regionid)
 	trimmed := generate.TrimPaths(connect, width, height)
-	game = GameData{width, height, trimmed}
+	game = GameData{width, height, trimmed, 0, 0}
 }
 
 func Render() {
 	gl.ClearColor(0, 0, 0, 1)
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-	gl.Color4f(0.5, 0, 0.5, 1)
+	gl.PushMatrix()
+	gl.Translatef(20-float32(game.PlayerX), 15-float32(game.PlayerY), 0)
 	gl.Begin(gl.TRIANGLES)
 	for i := 0; i < game.Width; i++ {
 	LOOP:
@@ -65,7 +67,16 @@ func Render() {
 			gl.Vertex3f(float32(i), float32(j+1), 0)
 		}
 	}
+	// Player
+	gl.Color4f(1, 0, 0, 1)
+	gl.Vertex3f(float32(game.PlayerX), float32(game.PlayerY), 0)
+	gl.Vertex3f(float32(game.PlayerX), float32(game.PlayerY+1), 0)
+	gl.Vertex3f(float32(game.PlayerX+1), float32(game.PlayerY), 0)
+	gl.Vertex3f(float32(game.PlayerX+1), float32(game.PlayerY+1), 0)
+	gl.Vertex3f(float32(game.PlayerX+1), float32(game.PlayerY), 0)
+	gl.Vertex3f(float32(game.PlayerX), float32(game.PlayerY+1), 0)
 	gl.End()
+	gl.PopMatrix()
 }
 
 func OnKey(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw.ModifierKey) {
@@ -74,9 +85,13 @@ func OnKey(window *glfw.Window, k glfw.Key, s int, action glfw.Action, mods glfw
 	}
 	switch k {
 	case glfw.KeyLeft:
+		game.PlayerX--
 	case glfw.KeyRight:
+		game.PlayerX++
 	case glfw.KeyUp:
+		game.PlayerY++
 	case glfw.KeyDown:
+		game.PlayerY--
 	case glfw.KeyEscape:
 		window.SetShouldClose(true)
 	}
