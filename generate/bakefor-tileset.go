39cff8613c7b1a22e41a1f2aefb5b6ef537a762e
geemili <minergeemili@gmail.com>
2016-02-02 17:12:30 -0600

    Changed and added some tiles.

--- generate/bakefor-tileset.go
+++ generate/bakefor-tileset.go
@@ -1,10 +1,12 @@
 package generate
 
 const (
-	hwall         = 220
-	vwall         = 171
+	hwall         = 76
+	vwall         = 59
 	period        = 226
-	leftDownElbow = 187
+	leftDownElbow = 251
+	leftUpElbow   = 157
+	rightUpElbow  = 12
 )
 
 func BakeForTileset(tiles []int, w, h int) []int {
@@ -17,6 +19,11 @@ func BakeForTileset(tiles []int, w, h int) []int {
 				getTile(i, j-1, tiles, w, h) == 0 &&
 				(getTile(i-1, j-1, tiles, w, h) != 0 || (getTile(i+1, j, tiles, w, h) != 0 && getTile(i, j+1, tiles, w, h) != 0)):
 				baked[(j*w)+i] = leftDownElbow
+			case getTile(i, j, tiles, w, h) == 0 &&
+				getTile(i-1, j, tiles, w, h) == 0 &&
+				getTile(i, j+1, tiles, w, h) == 0 &&
+				(getTile(i-1, j+1, tiles, w, h) != 0 || (getTile(i+1, j, tiles, w, h) != 0 && getTile(i, j-1, tiles, w, h) != 0)):
+				baked[(j*w)+i] = leftUpElbow
 			case getTile(i, j, tiles, w, h) == 0 &&
 				(getTile(i-1, j, tiles, w, h) != 0 || getTile(i+1, j, tiles, w, h) != 0):
 				baked[(j*w)+i] = vwall
